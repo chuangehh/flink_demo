f1465b81c58eb4a01c94808910c55ba60ee2caf6
liangchuanchuan <job_lcc@sina.com>
2020-06-09 09:12:49 +0800

    Flink: 脑图: 简介、快速上手、部署模式

--- src/main/java/org/example/wc/StreamWorldCount.java
+++ src/main/java/org/example/wc/StreamWorldCount.java
@@ -3,15 +3,19 @@ package org.example.wc;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 public class StreamWorldCount {
 
     public static void main(String[] args) throws Exception {
+        ParameterTool params = ParameterTool.fromArgs(args);
+        String hostname = params.get("hostname");
+        int port = params.getInt("port");
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        DataStreamSource<String> source = env.socketTextStream("localhost", 7777);
+        DataStreamSource<String> source = env.socketTextStream(hostname, port);
         source
                 .flatMap((FlatMapFunction<String, Tuple2<String, Integer>>) (value, out) -> {
                     String[] worlds = value.split("\\s+");
