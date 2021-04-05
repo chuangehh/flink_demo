f3bd3c1432c7c011fcd358b9b2d655769547aef0
liangchuanchuan <liangchuanchuan@banggood.com>
2021-04-05 15:09:41 +0800

    flink: 富函数, 分区

---
+++ src/main/java/org/example/api/PartitionTest.java
@@ -0,0 +1,39 @@
+package org.example.api;
+
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.util.Arrays;
+
+public class PartitionTest {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(4);
+        DataStream<SensorReading> source = env.fromCollection(Arrays.asList(
+                new SensorReading("guangzhou", "2021-04-01", 20.2)
+                , new SensorReading("guangzhou", "2021-04-02", 24.2)
+                , new SensorReading("guangzhou", "2021-04-03", 24.2)
+                , new SensorReading("guangzhou", "2021-04-04", 33.6)
+                , new SensorReading("guangzhou", "2021-04-05", 22.6)
+        ));
+
+
+        // shuffle 随机分配
+        // random.nextInt(numberOfChannels)
+        source.shuffle().print("shuffle");
+
+        // global 只给第一个下游
+        // return 0;
+        source.global().print("global");
+
+        // 根据 key分区
+        // keyGroupId * parallelism / maxParallelism
+        source.keyBy(SensorReading::getId).print("keyBy");
+
+        // TODO 自定义分区..
+
+        env.execute();
+    }
+
+}
---
+++ src/main/java/org/example/api/RichFunctionTest.java
@@ -0,0 +1,43 @@
+package org.example.api;
+
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.util.Arrays;
+
+public class RichFunctionTest {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(4);
+        DataStream<SensorReading> source = env.fromCollection(Arrays.asList(
+                new SensorReading("guangzhou", "2021-04-01", 20.2)
+                , new SensorReading("guangzhou", "2021-04-02", 24.2)
+                , new SensorReading("guangzhou", "2021-04-03", 24.2)
+                , new SensorReading("guangzhou", "2021-04-04", 33.6)
+                , new SensorReading("guangzhou", "2021-04-05", 22.6)
+        ));
+
+
+        source.map(new RichMapFunction<SensorReading, Tuple2<String, Double>>() {
+            @Override
+            public Tuple2<String, Double> map(SensorReading value) throws Exception {
+                return Tuple2.of(value.getId(), value.getTemperature());
+            }
+
+            @Override
+            public void open(Configuration parameters) throws Exception {
+                System.out.println("open");
+            }
+
+            @Override
+            public void close() throws Exception {
+                System.out.println("close");
+            }
+        }).print("rich");
+
+        env.execute();
+    }
+}
